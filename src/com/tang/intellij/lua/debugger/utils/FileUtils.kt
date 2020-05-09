6223e7b81b0331a61cff70fa2db2f017f7c3dce9
tangzx <love.tangzx@qq.com>
2020-05-09 23:48:54 +0800

    attach debugger

---
+++ src/com/tang/intellij/lua/debugger/utils/FileUtils.kt
@@ -0,0 +1,40 @@
+package com.tang.intellij.lua.debugger.utils
+
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import java.io.File
+
+object FileUtils {
+    private val pluginVirtualDirectory: VirtualFile?
+        get() {
+            val descriptor = PluginManager.getPlugin(PluginId.getId("com.tang.emmylua.attach-debugger"))
+            if (descriptor != null) {
+                val pluginPath = descriptor.path
+
+                val url = VfsUtil.pathToUrl(pluginPath.absolutePath)
+
+                return VirtualFileManager.getInstance().findFileByUrl(url)
+            }
+
+            return null
+        }
+
+    val archExeFile: String?
+        get() = getPluginVirtualFile("debugger/emmy/windows/x86/emmy_tool.exe")
+
+    fun getPluginVirtualFile(path: String): String? {
+        val directory = pluginVirtualDirectory
+        if (directory != null) {
+            var fullPath = directory.path + "/classes/" + path
+            if (File(fullPath).exists())
+                return fullPath
+            fullPath = directory.path + "/" + path
+            if (File(fullPath).exists())
+                return fullPath
+        }
+        return null
+    }
+}
